b561db65092ce730fbd5a641dc4a6ed2da8eda38
will_hoffmann <disabledngamer@gmail.com>
2016-01-09 11:55:36 +0000

    Remove status content and live row

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -9,55 +9,9 @@ function displayVisibleEntries(selector) {
 
 function loadLiveContent() {
   $.get("https://s3.amazonaws.com/charlieegan3/status.json", function(data) {
-    setLiveContent(JSON.parse(data));
     $('.spinner').remove();
     displayVisibleEntries('.live');
   });
-}
-
-function setLiveContent(data) {
-  $('#location-guess').text(data.metadata.most_recent_location);
-
-  $('#twitter-link').attr("href", data.tweet.link);
-  $('#twitter-content').text(data.tweet.text);
-  $('#twitter-meta').text(data.tweet.location + " - " + data.tweet.created_ago);
-  if (data.tweet.location != null) {
-    $('#twitter-meta').text(data.tweet.location + " - " + data.tweet.created_ago);
-  } else {
-    $('#twitter-meta').text(data.tweet.created_ago);
-  }
-
-  $("#strava-link").attr("href", data.activity.link);
-  $("#strava-distance").text(data.activity.distance);
-  $("#strava-name").text(data.activity.name);
-  $("#strava-duration").text(data.activity.moving_time);
-  if (data.activity.location != null) {
-    $("#strava-meta").text(data.activity.location + " - " + data.activity.created_ago);
-  } else {
-    $("#strava-meta").text(data.activity.created_ago);
-  }
-
-  $("#github-link").attr("href", data.commit.link);
-  $("#github-message").text("> " + data.commit.message);
-  $("#github-meta").text(data.commit.created_ago);
-
-  $("#lastfm-link").attr("href", data.track.link);
-  $("#lastfm-image").attr("src", data.track.images.large);
-  if (data.track.images.large == "") {
-    $("#lastfm-image").parent().remove();
-    $(".artist").parent().css('padding-left', "15px");
-  }
-  $("#lastfm-track").html(data.track.name);
-  $("#lastfm-artist").text(data.track.artist);
-  $("#lastfm-meta").text(data.track.created_ago);
-
-  $("#instagram-link").attr("href", data.image.link);
-  $("#instagram-image").attr("src", data.image.images.standard_resolution);
-  if (data.image.location != null) {
-    $("#instagram-meta").text(data.image.location + " - " + data.image.created_ago);
-  } else {
-    $("#instagram-meta").text(data.image.created_ago);
-  }
 }
 
 $(document).ready(function() {
